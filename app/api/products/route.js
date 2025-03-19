59ef263b92be0706f22b5867385d3b65406f2b33
BrutalOverkill <anvit19960@gmail.com>
2025-03-19 22:30:19 +0530

    Update route.js

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,7 +1,7 @@
 import Stripe from "stripe"
 import '../../../envConfig.js'
 
-const API_KEY = process.env.STRIPE_SECRET_KEY
+const API_KEY = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY
 const stripe = new Stripe(API_KEY, {
     apiVersion: '2023-10-16'
 })
@@ -42,3 +42,4 @@ export async function GET() {
         return Response.json({ error: 'Failed to fetch data from stripe' })
     }
 }
+
